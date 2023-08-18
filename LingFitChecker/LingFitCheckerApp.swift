4a4412cd661e5f5710ee69595e150f8c135a8d5a
Gitarupos <r.thousand325@i.softbank.jp>
2023-08-18 16:04:19 +0900

    Initial Commit

---
+++ LingFitChecker/LingFitCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LingFitCheckerApp.swift
+//  LingFitChecker
+//
+//  Created by tarupos_ on 2023/08/18.
+//
+
+import SwiftUI
+
+@main
+struct LingFitCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
