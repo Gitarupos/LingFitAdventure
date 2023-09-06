852af0d21e4fc1511a024ed46b11f6db6e8b07a8
Gitarupos <r.thousand325@i.softbank.jp>
2023-09-06 18:26:21 +0900

    progress

---
+++ LingFitChecker/SkillListOption.swift
@@ -0,0 +1,14 @@
+//
+//  SkillList.swift
+//  LingFitChecker
+//
+//  Created by tarupos_ on 2023/08/18.
+//
+
+import Foundation
+
+struct SkillList{
+    var id    : Int
+    var name  : String
+    var color : Int
+}
